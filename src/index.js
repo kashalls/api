222ab8b3dc6c1b0638a18f480813ea656ff2b4e1
Kashalls <kashalls@pm.me>
2022-10-16 02:11:20 -0700

    Do the mario!

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
+
 import Fastify from 'fastify'
+import mongodb from '@fastify/mongodb'
 const fastify = Fastify({
     logger: true,
     trustProxy: true
 })
+
+fastify.register(mongodb, { url: process.env.MONGODB })
 
 fastify.get("/", async (request, reply) => {
     return { hello: 'world' }
 })
 
 fastify.post('/eternal/balance', async (request, reply) => {
+    const balances = this.mongo.db.collection('balances')
+    const { balance, uuid } = request.body;
+    const result = await balances.insertOne({ balance, uuid, date: new Date() })
     console.log(request.body)
     return request.body
 })
